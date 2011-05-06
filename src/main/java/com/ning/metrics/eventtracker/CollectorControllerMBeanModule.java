cd63941f956a9b147f8cc6e6efab8e1f68a38c19
Pierre-Alexandre Meyer <pierre@ning.com>
2011-05-06 14:59:45 -0700

    guice: add missing CollectorControllerMBeanModule
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

---
+++ src/main/java/com/ning/metrics/eventtracker/CollectorControllerMBeanModule.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2010-2011 Ning, Inc.
+ *
+ * Ning licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.ning.metrics.eventtracker;
+
+import com.google.inject.AbstractModule;
+import org.weakref.jmx.guice.ExportBuilder;
+import org.weakref.jmx.guice.MBeanModule;
+
+/**
+ * Expose JMX properties for the eventtracker library.
+ * <p/>
+ * To enable them, install this module and configure jmxutils:
+ * install(new MBeanModule());
+ * // MBeanModule expects an MBeanServer to be bound, e.g. make sure to:
+ * binder().bind(MBeanServer.class).toInstance(ManagementFactory.getPlatformMBeanServer());
+ */
+public class CollectorControllerMBeanModule extends AbstractModule
+{
+    /**
+     * Configures a {@link com.google.inject.Binder} via the exposed methods.
+     */
+    @Override
+    protected void configure()
+    {
+        ExportBuilder builder = MBeanModule.newExporter(binder());
+
+        builder.export(ScribeSender.class).as("eventtracker:name=ScribeSender");
+        builder.export(CollectorController.class).as("eventtracker:name=CollectorController");
+    }
+}
